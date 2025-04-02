f8f35aaa53a32497ec35cfc1b7d6fb406a997ac5
yurlcho <yul115000@naver.com>
2025-04-02 15:41:40 +0900

    js

--- js/slide.js
+++ js/slide.js
@@ -1,12 +1,12 @@
 function prev(){
     $('.slide li:last').prependTo('.slide');
-    $('.slide').css('margin-left',-1920);
+    $('.slide').css('margin-left',"-100vw");
     // $('.slide').css({marginLeft-1920})
     $('.slide').stop().animate({marginLeft:0},800)
 }
 
 function next(){
-    $('.slide').stop().animate({marginLeft:-1920},800,function(){
+    $('.slide').stop().animate({marginLeft:"-100vw"},800,function(){
         $('.slide li:first').appendTo('.slide');
         $('.slide').css({marginLeft:0});
     });
